ee99600701de95c8459fdd0fc99d8ac7ba64cdf0
Petr Trofimov <pt@onliner.by>
2014-06-06 13:24:11 +0300

    hotkeys: fixed escape code check

--- src/Clockwork/Cli/Application.php
+++ src/Clockwork/Cli/Application.php
@@ -30,7 +30,7 @@ class Application
         $hotKey = 'a';
         $links = array();
         $since = microtime(true);
-        system("stty -echo -icanon min 0 time 0");
+        system('stty -echo -icanon min 0 time 0');
         echo $this->colors->colorize('{yellow}F{default}irst symbol in line - show details, {yellow}Backspace{default} - show requests for last 10 minutes, {yellow}Escape{default} - exit'), PHP_EOL;
         while (true) {
             foreach ($this->fileScanner->getNewFiles($since) as $file) {
@@ -42,18 +42,18 @@ class Application
                 $since = $file['time'];
             }
             usleep($this->updateInterval * 1000000);
-            foreach (str_split(fread(STDIN, 10)) as $char) {
+            $input = str_split(fread(STDIN, 100));
+            while ($char = array_shift($input)) {
                 if (ord($char) == 127) {
                     $since = microtime(true) - 600;
                     echo 'Requests for last 10 minutes', PHP_EOL;
-                } elseif (ord($char) == 27) {
-                    system("stty echo");
+                } elseif (ord($char) == 27 && empty($input)) {
+                    system('stty echo');
                     exit;
                 } elseif (isset($links[$char])) {
                     $this->details->output(json_decode(file_get_contents($links[$char]), true));
                 }
             }
         }
-
     }
 }
