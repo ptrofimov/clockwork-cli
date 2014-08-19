18820c695fffd4593340708f0160eed1e5778def
Petr Trofimov <pt@onliner.by>
2014-08-19 12:20:39 +0300

    clean exit on ctrl+c

--- src/Clockwork/Cli/Application.php
+++ src/Clockwork/Cli/Application.php
@@ -23,6 +23,16 @@ class Application
         $this->shortLine = new ShortLine;
         $this->details = new Details;
         $this->colors = new Colors;
+        if (function_exists('pcntl_signal')) {
+            declare(ticks = 1);
+            pcntl_signal(SIGINT, [$this, 'stop']);
+        }
+    }
+
+    public function stop()
+    {
+        system('stty echo icanon');
+        exit;
     }
 
     public function run()
@@ -31,7 +41,7 @@ class Application
         $links = array();
         $since = microtime(true);
         system('stty -echo -icanon min 0 time 0');
-        echo $this->colors->colorize('{yellow}F{default}irst symbol in line - show details, {yellow}Backspace{default} - show requests for last 10 minutes, {yellow}Escape{default} - exit'), PHP_EOL;
+        $this->printUsageLine();
         while (true) {
             foreach ($this->fileScanner->getNewFiles($since) as $file) {
                 $log = json_decode(file_get_contents($file['file']), true);
@@ -44,16 +54,25 @@ class Application
             usleep($this->updateInterval * 1000000);
             $input = str_split(fread(STDIN, 100));
             while ($char = array_shift($input)) {
-                if (ord($char) == 127) {
+                if (ord($char) == 127) { // Backspace
                     $since = microtime(true) - 600;
                     echo 'Requests for last 10 minutes', PHP_EOL;
-                } elseif (ord($char) == 27 && empty($input)) {
-                    system('stty echo');
-                    exit;
-                } elseif (isset($links[$char])) {
+                } elseif (ord($char) == 27 && empty($input)) { // Escape
+                    $this->stop();
+                } elseif (isset($links[$char])) { // Hotkey [a-z]
                     $this->details->output(json_decode(file_get_contents($links[$char]), true));
                 }
             }
         }
+    }
+
+    private function printUsageLine()
+    {
+        echo $this->colors->colorize(
+            '{yellow}F{default}irst symbol in line - show details, '
+            . '{yellow}Backspace{default} - show requests for last 10 minutes, '
+            . '{yellow}Escape{default} - exit'
+            . PHP_EOL
+        );
     }
 }
